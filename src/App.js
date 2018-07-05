9fef5f71ded8bf598b8ba2c39f5a848661a33720
AlgirdasM <algirdaslt@gmail.com>
2018-07-05 17:45:02 +0300

    feat: html/css design

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  toggleMenu = () => {
+    const menu = document.getElementById('menu');
+    const mapContainer = document.getElementById('mapContainer');
+
+    menu.classList.toggle('open');
+
+    if(menu.classList.contains('open')){
+      mapContainer.style.marginLeft = '300px';
+    } else {
+      mapContainer.style.marginLeft = '';
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+
+        <header>
+          <h1 tabIndex="0">Selected location</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <nav id="menu">
+
+          <button aria-label="menu" id="menu_button" onClick={() => this.toggleMenu()}></button>
+
+          <div id="menuHeader">
+            <h2>Logotype</h2>
+          </div>
+
+          <input tabIndex="0" aria-label="Filter list" id="filterInput" type="text" name="filter" placeholder="Enter location" />
+
+          <ul id="filteredList">
+            <li tabIndex="0">Kaunas City Museum</li>
+            <li tabIndex="0">Historical Presidential Palace</li>
+            <li tabIndex="0">Confluence Park</li>
+            <li tabIndex="0">House of Perkūnas</li>
+            <li tabIndex="0">Maironis Lithuanian Literature Museum</li>
+          </ul>
+
+        </nav>
+
+        <main id="mapContainer">
+          <div id="map" role="application" aria-hidden="true" aria-label="Google maps application"></div>
+        </main>
+
       </div>
     );
   }
