16f7fcb4eaad83c300fbe2498e131f4cafedf39e
AlgirdasM <algirdaslt@gmail.com>
2018-07-05 18:33:50 +0300

    feat: load google maps

--- src/App.js
+++ src/App.js
@@ -3,6 +3,55 @@ import './App.css';
 
 class App extends Component {
 
+  state = {
+    mapScriptLoading: true
+  }
+
+
+  componentDidMount() {
+    // Enable google to invoke initMap function
+    window.initMap = this.initMap;
+    // Add google maps script to body
+    this.addScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyAmgEg9okyBH-QCEAGu-UF2pIWVamfk7uU&v=3&callback=initMap')
+      .catch ( () => this.setState({ mapScriptLoading: false}) )
+  }
+
+  // add google maps script to body
+  addScript = (src) => {
+      return new Promise(function (resolve, reject) {
+          let script;
+
+          script = document.createElement('script');
+          script.async = true;
+          script.defer = true;
+          script.src = src;
+          script.onload = resolve;
+          script.onerror = reject;
+
+          document.body.appendChild(script);
+      });
+  }
+
+  // Init map function
+  initMap = () => {
+    let map;
+    const mapview = document.getElementById('map');
+
+    map = new window.google.maps.Map(mapview, {
+      center: {lat: 54.896779, lng: 23.886382},
+      zoom: 15,
+    });
+
+    const kaunasCityMuseum = {lat: 54.896779, lng: 23.886382};
+    // eslint-disable-next-line
+    const marker = new window.google.maps.Marker({
+      position: kaunasCityMuseum,
+      map: map,
+      title: 'Kaunas City Museum'
+    });
+  }
+
+  // Open and Close menu
   toggleMenu = () => {
     const menu = document.getElementById('menu');
     const mapContainer = document.getElementById('mapContainer');
@@ -45,7 +94,11 @@ class App extends Component {
         </nav>
 
         <main id="mapContainer">
+          { this.state.mapScriptLoading === false && (
+            <div className="error">Something went wrong... Please try again...</div>
+          )}
           <div id="map" role="application" aria-hidden="true" aria-label="Google maps application"></div>
+          
         </main>
 
       </div>
