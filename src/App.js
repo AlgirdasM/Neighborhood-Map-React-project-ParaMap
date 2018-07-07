d90ebb6a7458f60dd5a19e6ce157a19f9f131bf0
AlgirdasM <algirdaslt@gmail.com>
2018-07-07 12:40:47 +0300

    feat: list locations from variable and render them

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 class App extends Component {
 
   state = {
-    mapScriptLoading: true
+    mapScriptLoading: true,
+    locations: []
   }
 
 
@@ -13,7 +14,26 @@ class App extends Component {
     window.initMap = this.initMap;
     // Add google maps script to body
     this.addScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyAmgEg9okyBH-QCEAGu-UF2pIWVamfk7uU&v=3&callback=initMap')
-      .catch ( () => this.setState({ mapScriptLoading: false}) )
+      .catch ( () => this.setState({ mapScriptLoading: false}) );
+    // get location data
+    this.setState( {locations: this.getLocations()} )
+  }
+
+  // get location data
+  // TODO: get this data from external API
+  getLocations = () => {
+    const locations = [
+      {"title": "S. Darius and S. Girėnas Airport", "location": {"lat": 54.880278, "lng": 23.881944}, "icao": "EYKS"},
+      {"title": "Sasnava Airport", "location": {"lat": 54.662222, "lng": 23.452222}, "icao": "EYMM"},
+      {"title": "Alytus Airport", "location": {"lat": 54.413056, "lng": 24.056944}, "icao": "EYAL"},
+      {"title": "Molėtai Airport", "location": {"lat": 55.113056, "lng": 25.336667}, "icao": "EYMO"},
+      {"title": "Tauragė Airport", "location": {"lat": 55.231667, "lng": 22.150278}, "icao": "EYTR"},
+      {"title": "Šilutė Airport", "location": {"lat": 55.336944, "lng": 21.530556}, "icao": "EYSI"},
+      {"title": "Nida Airport", "location": {"lat": 55.327778, "lng": 21.045556}, "icao": "EYSI"},
+      {"title": "Paluknys Airport", "location": {"lat": 54.483056, "lng": 24.989722}, "icao": "EYSI"}
+    ];
+
+    return locations;
   }
 
   // add google maps script to body
@@ -34,21 +54,32 @@ class App extends Component {
 
   // Init map function
   initMap = () => {
+    const { google } = window;
+
     let map;
     const mapview = document.getElementById('map');
 
-    map = new window.google.maps.Map(mapview, {
+    map = new google.maps.Map(mapview, {
       center: {lat: 54.896779, lng: 23.886382},
       zoom: 15,
     });
 
     const kaunasCityMuseum = {lat: 54.896779, lng: 23.886382};
     // eslint-disable-next-line
-    const marker = new window.google.maps.Marker({
+    const marker = new google.maps.Marker({
       position: kaunasCityMuseum,
       map: map,
       title: 'Kaunas City Museum'
     });
+
+    const infowindow = new google.maps.InfoWindow({
+      content: 'Rotuses aikste'
+    });
+
+    marker.addListener('click', function(){
+      infowindow.open(map, marker);
+    }); 
+
   }
 
   // Open and Close menu
@@ -84,11 +115,10 @@ class App extends Component {
           <input tabIndex="0" aria-label="Filter list" id="filterInput" type="text" name="filter" placeholder="Enter location" />
 
           <ul id="filteredList">
-            <li tabIndex="0">Kaunas City Museum</li>
-            <li tabIndex="0">Historical Presidential Palace</li>
-            <li tabIndex="0">Confluence Park</li>
-            <li tabIndex="0">House of Perkūnas</li>
-            <li tabIndex="0">Maironis Lithuanian Literature Museum</li>
+            { this.state.locations &&( this.state.locations.map( location => (
+                <li key={location.title} tabIndex="0">{location.title}</li>
+              ))
+            )}
           </ul>
 
         </nav>
